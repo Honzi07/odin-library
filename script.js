0984d82f452cf21795a0b91d4b9b4322a062f757
Honzi07 <nyisztor.zsolt@gmail.com>
2023-07-01 09:16:57 +0200

    Function for form submitting than loop through the values and create a new book with the values

--- script.js
+++ script.js
@@ -2,10 +2,36 @@
 
 const myLibrary = [];
 
-function Book() {
+function Book(title, author, pages = 1, comment, read) {
   // the constructor...
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.comment = comment;
+  this.read = read;
 }
 
-function addBookToLibrary() {
+const testBook = new Book('The Hobbit', 'Tolkien', 241, '', true);
+
+function addBookToLibrary(book) {
   // do stuff here
+  myLibrary.push(book);
 }
+addBookToLibrary(testBook);
+
+function createBook() {
+  document.querySelector('.book-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const read = document.querySelector('#read').checked;
+    const [title, author, pages, comment] = [
+      ...document.querySelectorAll('input'),
+    ].map(input => input.value);
+    console.log(title, author, pages, comment, read);
+
+    const book = new Book(title, author, +pages, comment, read);
+    addBookToLibrary(book);
+    console.log(...myLibrary);
+  });
+}
+createBook();
